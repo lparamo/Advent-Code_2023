7f1289604465b00a0a4a1c049a8fd94b4d56bbe8
Lorea Páramo <lparamo002@ikasle.ehu.eus>
2023-12-09 10:12:45 +0100

    day2: part 2

---
+++ day2/day2_2.py
@@ -0,0 +1,28 @@
+#in each game you played, what is the fewest number of cubes of each color
+#that could have been in the bag to make the game possible?
+#The power of a set of cubes is equal to
+#the numbers of red, green, and blue cubes multiplied together.
+#What is the sum of the power of these sets?
+
+import re
+with open('inputDay2.txt') as file:
+    input=file.read().splitlines()
+
+sum=0   
+for line in input:
+    r=0
+    g=0
+    b=0
+    list=re.split(': |, |; | ',line)
+    for i in range(3,len(list),2):
+        if list[i]=='red':
+            if int(list[i-1])>r:
+                r=int(list[i-1])
+        elif list[i]=='green':
+            if int(list[i-1])>g:
+                g=int(list[i-1])
+        else:
+            if int(list[i-1])>b:
+                b=int(list[i-1])
+    sum+=r*g*b
+print(sum) 
